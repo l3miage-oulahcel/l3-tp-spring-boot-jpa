013d1604c56c58038f0be5343dbb09cf1139f770
l3miage-ouchanes <souleymen@ouchane.fr>
2023-03-05 12:05:25 +0100

    Association jakarta pour valider BookDTO (manque les restrictions sur isbn et language)

--- app/src/main/java/fr/uga/l3miage/library/books/BookDTO.java
+++ app/src/main/java/fr/uga/l3miage/library/books/BookDTO.java
@@ -1,9 +1,7 @@
 package fr.uga.l3miage.library.books;
 
 import fr.uga.l3miage.library.authors.AuthorDTO;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.*;
 
 import java.util.Collection;
 
@@ -11,10 +9,10 @@ import java.util.Collection;
 public record BookDTO(
         Long id,
         @NotBlank String title,
-        long isbn,
+        long isbn, // 13 chiffres
         @NotBlank String publisher,
-        short year,
-        String language,
+        @Min(100) @Max(2023) short year,
+        String language, //
         Collection<AuthorDTO> authors
 ) {
 }
